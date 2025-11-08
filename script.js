ec351d5035ab59646b1e7ee2ead126c41f7969ff
Tasos Andreou <tasosandreou01@gmail.com>
2025-11-08 19:26:10 +0200

    initialized vite react + compiler

--- script.js
+++
@@ -1,8 +0,0 @@
-var box = document.getElementById('box');
-box.addEventListener('mouseover', function() {
-  this.classList.add('hovered');
-});
-
-box.addEventListener('mouseout', function() {
-  this.classList.remove('hovered');
-});
